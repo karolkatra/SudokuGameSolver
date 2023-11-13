b36d4bb1bafcbfafbcc41761e29852d93baa295b
karolkatra <150198018+karolkatra@users.noreply.github.com>
2023-11-13 11:54:11 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+def print_board(board):
+    for row in board:
+        print(" ".join(map(str, row)))
+
+
+def is_valid(board, row, col, num):
+    if num in board[row]:
+        return False
+
+    if num in [board[i][col] for i in range(9)]:
+        return False
+
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+
+    return True
+
+
+def find_empty_location(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+    return None
+
+
+def solve_sudoku(board):
+    empty = find_empty_location(board)
+
+    if not empty:
+        return True
+
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+if __name__ == "__main__":
+    sudoku_board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    if solve_sudoku(sudoku_board):
+        print("\nRozwiązane sudoku:")
+        print_board(sudoku_board)
+    else:
+        print("\nBrak rozwiązania dla sudoku.")
